004a68102b22f4e9cc2d54718cc4c9415ecd9e33
Ignacio Velasquez <ivelasquez@nisum.com>
2015-02-19 18:33:25 -0300

    horizontal

--- src/main/com/example/SampleReport.java
+++ src/main/com/example/SampleReport.java
@@ -15,6 +15,8 @@ import net.sf.dynamicreports.report.builder.component.RectangleBuilder;
 import net.sf.dynamicreports.report.builder.style.StyleBuilder;
 import net.sf.dynamicreports.report.constant.HorizontalAlignment;
 import net.sf.dynamicreports.report.constant.ImageScale;
+import net.sf.dynamicreports.report.constant.PageOrientation;
+import net.sf.dynamicreports.report.constant.PageType;
 import net.sf.dynamicreports.report.constant.VerticalAlignment;
 import net.sf.dynamicreports.report.exception.DRException;
 
@@ -24,7 +26,7 @@ public class SampleReport {
 	public SampleReport() {
 		try {
 			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection("jdbc:postgresql://NI-NTB-064:5432/modelpos",
+			connection = DriverManager.getConnection("jdbc:postgresql://NI-NTB-010:5432/onboarding",
 					"postgres", "postgres");
 			build();
 		} catch (SQLException e) {
@@ -35,36 +37,54 @@ public class SampleReport {
 	}
 
 	private void build() {
-		StyleBuilder style = stl.style().setVerticalAlignment(VerticalAlignment.MIDDLE).setRadius(10)
-				.setBackgroundColor(new Color(109, 158, 235))
+		StyleBuilder style = stl.style().setVerticalAlignment(VerticalAlignment.MIDDLE)
+				.setRadius(10).setBackgroundColor(new Color(173, 216, 230))
 				.setLinePen(stl.pen().setLineColor(Color.LIGHT_GRAY));
 		RectangleBuilder background = cmp.rectangle().setStyle(style)
 				.setPrintWhenExpression(exp.printInOddRow());
 
 		try {
-			report().setColumnStyle(Templates.columnStyle)
+			report().setPageFormat(PageType.A4, PageOrientation.LANDSCAPE)
+					.setColumnStyle(Templates.columnStyle)
 					.setColumnTitleStyle(Templates.boldCenteredStyle)
 					.setColumnHeaderBackgroundComponent(background)
 					.setDetailBackgroundComponent(background)
 					.columns(
-							col.column("Id", "id", type.integerType()).setHorizontalAlignment(
+							col.column("Participant Name", "participantname", type.stringType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Program Description", "programdescription",
+									type.stringType()).setHorizontalAlignment(
 									HorizontalAlignment.CENTER),
-							col.column("Name", "name", type.stringType()).setHorizontalAlignment(
-									HorizontalAlignment.CENTER),
-							col.column("Id Number", "id_number", type.stringType())
+							col.column("Start Date", "startdate", type.dateType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Status", "programstatus", type.stringType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Asigned Tasks", "asignedtasks", type.integerType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Started Tasks", "startedtasks", type.integerType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Tasks in Progress", "inprogresstasks", type.integerType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Completed Tasks", "completedtasks", type.integerType())
+									.setHorizontalAlignment(HorizontalAlignment.CENTER),
+							col.column("Total Tasks", "totaltasks", type.integerType())
 									.setHorizontalAlignment(HorizontalAlignment.CENTER))
 					.title(cmp.verticalList().add(
 							cmp.image("src/resources/logo.png")
-									.setHorizontalAlignment(HorizontalAlignment.CENTER).setDimension(50, 50),
+									.setHorizontalAlignment(HorizontalAlignment.CENTER)
+									.setDimension(50, 50),
 							Components
-									.text("Clients")
+									.text("On-boarding Progress Status Report")
 									.setStyle(
 											Templates.bold18CenteredStyle.setForegroundColor(
-													new Color(109, 158, 235)).setBackgroundColor(
+													new Color(58, 148, 210)).setBackgroundColor(
 													new Color(255, 255, 255)))
 									.setHorizontalAlignment(HorizontalAlignment.CENTER)
-									.setHeight(35))).pageFooter(Templates.footerComponent)
-					.setDataSource("SELECT id, name, id_number FROM client", connection).show();
+									.setHeight(35)))
+					.pageFooter(Templates.footerComponent)
+					.setDataSource(
+							"select concat(participant.name, ' ', participant.lastname) as participantname, program.description as programdescription, program.started as startdate, program.status as programstatus, (select count(task.id) from task where task.status = 'ASIGNED' and task.program_id = program.id) as asignedtasks, (select count(task.id) from task where task.status = 'STARTED' and task.program_id = program.id) as startedtasks, (select count(task.id) from task where task.status = 'IN_PROGRESS' and task.program_id = program.id) as inprogresstasks, (select count(task.id) from task where task.status = 'COMPLETED' and task.program_id = program.id) as completedtasks, count(task.id) as totaltasks from participant join program on participant.id = program.participant_id left join task on program.id = task.program_id group by participantname, programdescription, programstatus, startdate, program.id",
+							connection).show();
 			// .toPdf(Exporters.pdfExporter(new File("asdasd.pdf")));
 		} catch (DRException e) {
 			e.printStackTrace();
